b5d870c97190320132dcc5233542c29e577b6651
Tylryan <tlrrn5@gmail.com>
2021-02-21 07:11:07 -0600

    Backing up my sh files

--- fresh_start.sh
+++ fresh_start.sh
@@ -3,25 +3,25 @@
 options=$1
 
 # Make A bin directory
-mkdir ~/bin
+mkdir ~/bin || :
 
 # Make the files executable
 chmod 744 `ls`
 
 # Move the files to bin
-cp -r `ls` ~/bin
+cp -r `ls` ~/bin &&
 
 # Let bash know you want to read binaries from that location
 PATH=$PATH:$HSME/bin
 
-if [[ $options =~ "r" ]] && [[ $options =~ "R" ]];then
+if [[ $options =~ "r" ]] && [[ $options =~ "R" ]];then # Works 02/21/21
     cd .. ; rm -rf bash_repository/
     reboot
 	
-elif [[ $options == "-R" ]];then
+elif [[ $options == "-R" ]];then #This works 02/21/21
         # Removing the original bash_repository
     cd .. ; rm -rf bash_repository/
-elif [[ $options == "-r" ]];then
+elif [[ $options == "-r" ]];then # Works 02/21/21
 	#Reboot your computer
 	reboot
 fi
