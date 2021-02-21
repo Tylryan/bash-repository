a5c1630e2acb331e3d71a63846f8675b3cf414c5
Tylryan <tlrrn5@gmail.com>
2021-02-21 07:16:07 -0600

    Backing up my sh files

--- fresh_start.sh
+++ fresh_start.sh
@@ -2,14 +2,11 @@
 
 options=$1
 
-# Make A bin directory
-mkdir ~/bin || :
-
 # Make the files executable
 chmod 744 `ls`
 
-# Move the files to bin
-cp -r `ls` ~/bin &&
+# Copy the files to bin if there. If not, make them and move there.
+cp -r `ls` ~/bin || mkdir ~/bin ; cp -r `ls` ~/bin &&
 
 # Let bash know you want to read binaries from that location
 PATH=$PATH:$HSME/bin
