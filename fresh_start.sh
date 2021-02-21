4531ebf02d667bc78560f58e6229993ba540184a
Tylryan <tlrrn5@gmail.com>
2021-02-21 05:11:28 -0600

    Backing up my sh files

--- fresh_start.sh
+++ fresh_start.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/bash
 
+remove_repository=$1
+
 # Make A bin directory
 #mkdir ~/bin
 
@@ -12,5 +14,11 @@
 # Let bash know you want to read binaries from that location
 #PATH=$PATH:$HSME/bin
 
+# Removing the original repository since it is not technically needed anymore
+if [ $1 == "-r" ];then
+	# Going back a directory
+	cd ..
+	# Removing bash_repository
+	rm -rf bash_repository/
 # Reboot your computer
 #reboot
