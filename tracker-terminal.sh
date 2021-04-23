6643ea125e8b4b4e29bd14be835fb64d9011d933
Tylryan <tlrrn5@gmail.com>
2021-04-23 18:13:08 -0500

    Backing up my sh files on Fri Apr 23 06:13:08 PM CDT 2021

---
+++ tracker-terminal.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+
+cd $(find ~/ -name tracker) && python3 main.py
+
+
--- tracker.sh
+++ tracker.sh
@@ -1,5 +1,11 @@
-#! /usr/bin/bash
+#!/usr/bin/bash
 
-cd $(find ~/ -name tracker) && python3 main.py
-
-
+subject=$1
+date=$2
+hours=$3
+if [ ! -z ${subject} ] && [ ! -z ${date} ] && [ ! -z ${hours} ]
+	then
+		echo "${subject},${date},${hours}" >> $(find ~/Documents -iname records.csv)
+else 
+	echo "You need to have a Subject Date and Hours in that order."
+fi
