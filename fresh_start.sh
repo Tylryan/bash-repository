ce625d6ee6c089532b6d4ae2c0918036128106da
Tylryan <tlrrn5@gmail.com>
2021-05-05 14:44:45 -0500

    Backing up my sh files on Wed May  5 02:44:45 PM CDT 2021

--- fresh_start.sh
+++ fresh_start.sh
@@ -8,16 +8,29 @@ chmod 744 `ls`
 # Copy the files to bin if there. If not, make them and move there.
 cp -r `ls` ~/bin || mkdir ~/bin ; cp -r `ls` ~/bin &&
 
-# Let bash know you want to read binaries from that location
-PATH=$PATH:$HSME/bin
+# Checks to see if you have zsh
+case $SHELL in 
+	/usr/bin/zsh)
+		# Let bash know you want to read binaries from that location
+		echo "PATH=$PATH:$HOME/bin" >> ~/.zshrc
+		;;
+	*)
+		echo "PATH=$PATH:$HOME/bin" >> ~/.bashrc
+		;;
+esac
 
+
+# Remove the original bash_repository and reboot.
 if [[ $options =~ "r" ]] && [[ $options =~ "R" ]];then # Works 02/21/21
     cd .. ; rm -rf bash_repository/
     reboot
 	
+# Remove the original bash repository, but no reboot.
 elif [[ $options == "-R" ]];then #This works 02/21/21
         # Removing the original bash_repository
     cd .. ; rm -rf bash_repository/
+
+# Reboot, but don't remove the origina bash repository.
 elif [[ $options == "-r" ]];then # Works 02/21/21
 	#Reboot your computer
 	reboot
