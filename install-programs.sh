1056362abf0b90a54ae9968c17643de209a8eff7
Tyler <tlrrn5@gmail.com>
2021-07-28 19:16:49 -0500

    Working out some kinks

--- install-programs.sh
+++ install-programs.sh
@@ -1,13 +1,26 @@
-# Update
+#               Update
+cd
 sudo pacman -Syu
 
-# Putting all my files back
-git clone https://github.com/Tylryan/bash_repository.git
-./bash_repository/install.sh && ~/bin/git-ssh-setup.sh
+#               Putting all my files back
+./bin/git-ssh-setup.sh
 rm -rf ~/.config/bspwm/ 
 git clone git@github.com:Tylryan/bspwm-config.git bspwm && mv bspwm ~/.config
+
+#               ZSH SETUP
 git clone git@github.com:Tylryan/zsh_config.git zsh ; mv zsh ~/.config
-zsh
+
+cp .config/zsh/.p10k.zsh ~/
+cp .config/zsh/.zshrc ~/
+
+#               Syntax Highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
+#               Auto-Suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins/zsh-autosuggestions
+#               Powerlevel10k: Customized PS1
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/plugins/powerlevel10k
+
+
 git clone git@github.com:Tylryan/task_warrior_backup.git task && mv task .config/
 git clone git@github.com:Tylryan/nvim_backup.git
 
@@ -27,7 +40,7 @@ yay -S brave-bin eclipse-java-bin
 
 # DOOM EMACS
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-~/.emacs.d/bin/doom instally
+~/.emacs.d/bin/doom install
 
 cd .emacs.d/ ; ./doom sync
 
