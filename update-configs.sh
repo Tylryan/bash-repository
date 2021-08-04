c3cc67a5c40a1d324c93782a2151297881388d31
Tyler <t>
2021-08-04 09:21:27 -0500

    Auto-commit on Wednesday, 08-04-2021, at 09:21 AM

--- update-configs.sh
+++ update-configs.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/bash
 gp=`~/bin/gp.sh`
-cd ~/bin && ${gp}
+cd ~/bin && ${gp} &
 sleep 5
 echo "Done"
-cd ~/Notes && ${gp}
+cd ~/Notes && ${gp}&
 sleep 5
 echo "Done"
 cp -r ~/.config/polybar/* ~/.config/bspwm/polybar.outer
 echo "Done"
 cp -r ~/.config/sxhkd/* ~/.config/bspwm/sxhkd1
 echo "Done"
-cd ~/.config/bspwm && ${gp}
+cd ~/.config/bspwm && ${gp} &
 sleep 5
 echo "Done"
-cd ~/.config/nvim && ${gp}
+cd ~/.config/nvim && ${gp}&
 sleep 5
 echo "Done"
-cd ~/.config/task && ${gp}
+cd ~/.config/task && ${gp} &
 sleep 5
 echo "Done"
 # Don't need to cp. I have them linked
-cd ~/.config/zsh/ && ${gp}
+cd ~/.config/zsh/ && ${gp} &
 sleep 5
 echo "Done"
