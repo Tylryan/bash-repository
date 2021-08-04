453e2d7609d3ecc19f701f642a10ef73ede9f0f3
Tyler <t>
2021-08-04 09:30:28 -0500

    Auto Commit

--- update-configs.sh
+++ update-configs.sh
@@ -1,26 +1,11 @@
 #!/usr/bin/bash
-export GIT_SSL_NO_VERIFY=1
-gp=`~/bin/gp.sh`
-cd ~/bin && ${gp} &
-sleep 5
-cd ~/Notes && sleep 2 && pwd && ${gp}&
-sleep 5
-echo "Done"
-cp -r ~/.config/polybar/* ~/.config/bspwm/polybar.outer
-echo "Done"
-cp -r ~/.config/sxhkd/* ~/.config/bspwm/sxhkd1
-echo "Done"
-cd ~/.config/bspwm && ${gp} &
-sleep 5
-echo "Done"
-cd ~/.config/nvim && ${gp}&
-sleep 5
-echo "Done"
-cd ~/.config/task && ${gp} &
-sleep 5
-echo "Done"
+gp=`git add . ; git commit -m "Auto Commit" ; git push`
+cd ~/bin && ${gp} ;
+cd ~/Notes && ${gp} ;
+cp -r ~/.config/polybar/* ~/.config/bspwm/polybar.outer ;
+cp -r ~/.config/sxhkd/* ~/.config/bspwm/sxhkd1 ;
+cd ~/.config/bspwm && ${gp} ;
+cd ~/.config/nvim && ${gp} ;
+cd ~/.config/task && ${gp} ;
 # Don't need to cp. I have them linked
-cd ~/.config/zsh/ && ${gp} &
-sleep 5
-echo "Done"
-export GIT_SSL_NO_VERIFY=0
+cd ~/.config/zsh/ && ${gp} ;
