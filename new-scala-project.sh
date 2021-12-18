6789181e3f28f03bf55f89251d8674487ae6f70f
Tyler Ryan <tlrrn5@gmail.com>
2021-12-18 02:25:08 -0600

    Didn't really do much honestly

--- new-scala-project.sh
+++ new-scala-project.sh
@@ -1,19 +1,26 @@
-sbt new scala/hello-world.g8 -o $1
-echo '
+sbtBuildSnippet='
 Global / onChangedBuildSource := ReloadOnSourceChanges
 libraryDependencies ++= Seq(
 "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.2"
 )
-lazy val any_name = (project in file(".")) // Determines the baseDirectory
+lazy val root = (project in file(".")) // Determines the baseDirectory
     .settings(
         scalaVersion := "2.13.3",
-        name := "any_name_too",
-        version := "1.0",
+        name := "default", // Change this to whatever you want
+        version := "0.0.0",
         Compile / mainClass := Some("Main"), // The name of your Main File
         assembly / mainClass := Some("Main"),
         // Sweet Sweet Compile Warnings
         Compile / console / scalacOptions := Seq(
         "-language:_", "-depreciation","-Xlint", "-Ywarm-unused",
         "-Yawrn-dead-code", "-explaintypes", "opt:_", "-Xdev")
-    )' > $1/build.sbt
+    )' 
+
+if [[ -z $1 ]];then
+    echo "Enter the name of the project"
+    echo "E.g. ssc hello-world"
+    exit 1
+fi
+sbt new scala/hello-world.g8 --name=$1
+echo $sbtBuildSnippet > $1/build.sbt
 echo 'addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.15.0")' > $1/project/plugins.sbt
