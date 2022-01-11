17cec9086a30a0cea7febc071f6a580ac58d2c96
tyler ryan <tlrrn5@gmail.com>
2022-01-11 13:28:49 -0600

    modularized the ssh-setup

--- backup.sh
+++ backup.sh
@@ -1,22 +1,26 @@
-#!/usr/bin/python3.9
+#!/usr/bin/bash
 
-# Takes about 5 minutes
-backup_configs(){
+# Takes about 5-10 minutes
+
 backup_dirs=(
-    alacritty awesome bspwm nvim polybar task zsh
+    alacritty awesome bspwm nvim polybar task zsh xfce4/terminal
 )
-
-for dir in ${backup_dirs[@]}
-do
+backup_configs(){
+for dir in ${backup_dirs[@]};do
     sudo cp -ru ~/.config/$dir ~/Ex-Drive/Dotfiles
 done
-
 }
 
 backup_documents(){
-    sudo cp -ru ~/Documents ~/Ex-Drive/Documents
-    sudo cp -ru ~/Pictures ~/Ex-Drive/Pictures
-    sudo cp -ru ~/Bin ~/Ex-Drive/Bin
+    sudo cp -ru ~/Documents ~/Ex-Drive/
+    sudo cp -ru ~/Pictures ~/Ex-Drive/
+    sudo cp -ru ~/Bin ~/Ex-Drive/
+    sudo cp -ru ~/Notes ~/Ex-Drive/
+    sudo cp -ru ~/Applications ~/Ex-Drive/
+
+    sudo cp -ru ~/.elinks ~/Ex-Drive/Dotfiles/elinks 
+
+
 }
 
 backup_configs
--- ssh-setup.sh
+++ ssh-setup.sh
@@ -1,50 +1,78 @@
 #!/usr/bin/bash
-cont=true
-while [ $cont = true ]
-do
-    echo "Enter Your Email Address: "
-    read email
-    echo "Is this correct? y/[N] --> ${email}"
-    read response
-    go=[Yy]
-    if [[ $response =~ $go ]];then
-        cont=false
-    fi
-done
-ssh-keygen -t ed25519 -C \"$email\" 
-wait 1
-eval "$(ssh-agent -s)"
-wait 1
-ssh-add ~/.ssh/id_ed25519
 
-echo "**********************************"
-echo "Which distro do you have? ([a]rch or [d]ebian): "
-read distro
-echo "**********************************"
-case $distro in
-    [aA])
-        sudo pacman -S xclip
-        xclip -selection clipboard < ~/.ssh/id_ed25519.pub
-        ;;
-    [dD])
-        sudo apt install xclip
-        xclip -selection clipboard < ~/.ssh/id_ed25519.pub
-        ;;
-    *)
-        echo "Please Select Either ARCH or DEBIAN Next Time"
-        wait 2
-        ;;
-esac
-echo "**********************************"
-echo "What is your First Name? "
-read first 
-echo "What is your Last Name? "
-read last
-echo "**********************************"
-echo "**********************************"
-echo "Public Key copied to clipboard..."
-echo "**********************************"
-echo "**********************************"
-echo "Go paste that key into Github, then copy the following to your terminal.\n"
-git config --global user.email "$email"
-git config --global user.name "$first $last"
+# A script that automates the ssh key generation and copies the pub file to your clipboard
+
+EXIT_SUCCESS=0
+EXIT_FAILURE=1
+SUCCESS=1
+cont=true
+
+ssh_setup() {
+    get_user_info
+    create_ssh_keys
+    copy_ssh_pub_to_clipboard
+    setup_git_config
+
+    return $SUCCESS
+}
+
+get_user_info() {
+    while [[ $cont == true ]]
+    do
+        read -r -p "Enter Your Email Address: " email
+        read -r -p "Is this correct? y/[N] --> ${email}: " response
+        is_correct_email=[Yy]
+        if [[ $response = $is_correct_email ]];then 
+            cont=false
+        fi
+    done
+    return $SUCCESS
+}
+
+create_ssh_keys() {
+    ssh-keygen -t ed25519 -C \"$email\" 
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/id_ed25519
+    return $SUCCESS
+}
+
+copy_ssh_pub_to_clipboard() {
+    echo "******************************************************"
+    read -r -p "Which distro do you have? ([a]rch or [d]ebian): " distro
+    echo "******************************************************"
+    case $distro in
+        [aA] | [aA]rch)
+            sudo pacman -S xclip
+            xclip -selection clipboard < ~/.ssh/id_ed25519.pub
+            ;;
+        [dD] | [dD]ebian)
+            sudo apt install xclip
+            xclip -selection clipboard < ~/.ssh/id_ed25519.pub
+            ;;
+        *)
+            echo "Please Select Either ARCH or DEBIAN Next Time"
+            wait 2
+            ;;
+    esac
+    return $SUCCESS
+}
+
+setup_git_config() {
+    echo "**********************************"
+    read -r -p "What is your First Name? " first_name
+    read -r -p "What is your Last Name? " last_name
+    echo "**********************************"
+    echo "**********************************"
+    echo "Public Key copied to clipboard..."
+    echo "**********************************"
+    echo "**********************************"
+    echo -e "Go paste that key into Github, then copy the following to your terminal.\n"
+
+    git config --global user.email $email
+    git config --global user.name "$first_name $last_name"
+    return $SUCCESS
+}
+
+
+ssh_setup
+exit $EXIT_SUCCESS
